329f940b4f1dc291b3a6574978dccc78872e3bad
caclairechen <ca.claire.chen@gmail.com>
2020-02-24 22:23:53 -0600

    simple loop for user input

---
+++ src/main.py
@@ -0,0 +1,8 @@
+print ("Welcome to GPAMotivator!\n\n")
+
+while True:
+	user_input = input("Enter your message (enter to exit): ")
+	if user_input == "":
+		break
+	else:
+		print("You entered: "+user_input)
