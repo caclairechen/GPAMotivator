fcb844d2a39538789673868dceb68480eaed2cc3
caclairechen <ca.claire.chen@gmail.com>
2020-02-26 13:04:23 -0600

    move duplicate functions to helper and add null checking

---
+++ src/helper.py
@@ -0,0 +1,30 @@
+from course import Course
+
+def printCoursesList(courses, withAssessments = False):
+	for course in courses:
+		print(course.name+" "+course.target_gpa)
+		if withAssessments:
+			for assess in course.assessments:
+				print("\t"+assess.name+" "+assess.portion)
+
+def findCourseFromList(courses, course_name):
+	for course in courses:
+		if course.name == course_name:
+			return course
+
+def addCourseToList(courses, course_name, course_target_gpa):
+	new_course = Course(course_name, course_target_gpa)
+	courses.append(new_course)
+
+def removeCourseFromList(courses, course_name):
+	course = findCourseFromList(courses, course_name)
+	if course:
+		courses.remove(course)
+
+def addAssessmentToCourse(course, assess_name, assess_portion):
+ 	course.addAssessment(assess_name, assess_portion)
+
+def removeAssesssmentFromCourse(courses, course_name, assess_name):
+	course = findCourseFromList(courses, course_name)
+	if course:
+		course.removeAssessment(assess_name)
